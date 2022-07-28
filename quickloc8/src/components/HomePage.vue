cbc0276d199c852905fa94e979fcc3e69e906f1e
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2022-07-28 22:23:20 +0200

    SPLASH SCREEN ADDED

---
+++ quickloc8/src/components/HomePage.vue
@@ -0,0 +1,49 @@
+
+
+<template>
+ <div id="loading" ref="loading"> 
+ <img alt="Oooops" src="../assets/quickloc8.svg"/>
+ <h3> Loading.....</h3>
+ <vue-loaders-ball-beat/>
+ </div>
+</template>
+
+<script>
+//import loaders from npm
+
+
+
+export default {
+  name: 'HomePage',
+ 
+
+  mounted(){
+  const load = this.$refs.loading;
+  console.log(load);
+  setTimeout(()=>{
+    load.style.display="none";
+  },3500)
+  }
+
+
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
